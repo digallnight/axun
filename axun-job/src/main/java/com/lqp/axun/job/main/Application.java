eb5b972b6b0408f390a682a13d88a9e44d7fa217
xiaoyong911120@126.com <github1713>
2018-08-22 20:57:36 +0800

    tz

--- axun-job/src/main/java/com/lqp/axun/job/main/Application.java
+++ axun-job/src/main/java/com/lqp/axun/job/main/Application.java
@@ -1,20 +1,18 @@
 package com.lqp.axun.job.main;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- * @Auther: xxiao
- * @Date: 2018/8/22 00:01
- * @Description:
- */
-@RestController
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@Slf4j
+@EnableScheduling
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info("start");
     }
 }
---
+++ axun-job/src/main/java/com/lqp/axun/job/service/ScheduledTasks.java
@@ -0,0 +1,59 @@
+package com.lqp.axun.job.service;
+/**
+ * 简单的spirng定时任务示例
+ */
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+//@EnableScheduling：标注启动定时任务。
+//@Scheduled(fixedRate = 1000 * 30)  定义某个定时任务。
+@Component
+public class ScheduledTasks {
+
+    @Scheduled(fixedRate = 100)
+    public void reportCurrentTime() {
+        System.out.println("Scheduling Tasks Examples: The time is now " + dateFormat().format(new Date()));
+    }
+
+    /**
+     * cronExpression的配置说明
+     * 字段   允许值   允许的特殊字符
+     * 秒    0-59    , - * /
+     * 分    0-59    , - * /
+     * 小时    0-23    , - * /
+     * 日期    1-31    , - * ? / L W C
+     * 月份    1-12 或者 JAN-DEC    , - * /
+     * 星期    1-7 或者 SUN-SAT    , - * ? / L C #
+     * 年（可选）    留空, 1970-2099    , - * /
+     * - 区间
+     * * 通配符
+     * ? 你不想设置那个字段
+     * 下面只例出几个式子
+     *
+     * CRON表达式    含义
+     * "0 0 12 * * ?"    每天中午十二点触发
+     * "0 15 10 ? * *"    每天早上10：15触发
+     * "0 15 10 * * ?"    每天早上10：15触发
+     * "0 15 10 * * ? *"    每天早上10：15触发
+     * "0 15 10 * * ? 2005"    2005年的每天早上10：15触发
+     * "0 * 14 * * ?"    每天从下午2点开始到2点59分每分钟一次触发
+     * "0 0/5 14 * * ?"    每天从下午2点开始到2：55分结束每5分钟一次触发
+     * "0 0/5 14,18 * * ?"    每天的下午2点至2：55和6点至6点55分两个时间段内每5分钟一次触发
+     * "0 0-5 14 * * ?"    每天14:00至14:05每分钟一次触发
+     * "0 10,44 14 ? 3 WED"    三月的每周三的14：10和14：44触发
+     * "0 15 10 ? * MON-FRI"    每个周一、周二、周三、周四、周五的10：15触发
+     */
+    //每1分钟执行一次
+    @Scheduled(cron = "0 */1 *  * * * ")
+    public void reportCurrentByCron() {
+        System.out.println("Scheduling Tasks Examples By Cron: The time is now " + dateFormat().format(new Date()));
+    }
+
+    private SimpleDateFormat dateFormat() {
+        return new SimpleDateFormat("HH:mm:ss");
+    }
+
+}
